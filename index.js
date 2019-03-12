a56ec9026b36f0cb487ed8b3942152ab03217cfd
dubaua <dubaua@gmail.com>
2019-03-12 14:18:24 +0500

    draw given example data

--- index.js
+++ index.js
@@ -1,44 +1,109 @@
-const chart = document.getElementById('chart');
+const STROKE_WIDTH = 2;
+const STROKE_LINECAP = 'square';
+const MS_IN_DAY = 1000 * 60 * 60 * 24;
+const app = document.getElementById('chart-app');
 
-const chartWidth = window.innerWidth - 16;
+const chartWidth = window.innerWidth;
 const chartHeight = 150;
 
-chart.setAttribute('width', chartWidth);
-chart.setAttribute('height', chartHeight);
-chart.setAttribute('viewBox', `0 0 ${chartWidth} ${chartHeight}`);
+createChart(chart_data[0]);
 
-const ascending = (a,b) => a-b;
-const descending = (a,b) => b-a;
-
-const data = chart_data[0];
-
-// data already sorted by date
-
-// draw chart
-const dataX = data.map(point => point.result).sort(ascending);
-const minX = dataX[0];
-const maxX = dataX[dataX.length - 1];
-
-const dataY = data.map(point => point.chance).sort(ascending);
-const minY = dataY[0];
-const maxY = dataY[dataX.length - 1];
-
-function collectPath(path, point) {
-  const x = (point.result - minX) / (maxX - minX) * chartWidth;
-  const y = (maxY - point.chance) / (maxY - minY) * chartHeight;
-  if (path === '') {
-    return `M${x} ${y}`;
+function createChart(chartData){
+  const { columns, types, names, colors } = chartData;
+  
+  let chart = {
+    x: null,
+    y: [],
+  };
+  
+  for (const key in types) {
+    if (types.hasOwnProperty(key)) {
+      const type = types[key];
+      switch (type) {
+        case 'x':
+          chart.x = getColumnByKey(columns, key);
+          break;
+        case 'line':
+          chart.y.push({
+            key,
+            data: getColumnByKey(columns, key),
+            color: colors[key],
+            name: names[key],
+          });
+          break;
+      }
+    }
   }
-  return `${path}L${x} ${y}`;
+  
+  const chartWrapperEl = createElement('section');
+  const chartSvg = createElementNS('svg');
+  chartSvg.setAttribute('width', chartWidth);
+  chartSvg.setAttribute('height', chartHeight);
+  chartSvg.setAttribute('viewBox', [0, 0, chartWidth, chartHeight].join(' '));
+  chartSvg.setAttribute('fill', 'none');
+  
+  const allY = [].concat(...chart.y.map(line => line.data));
+  // const minY = Math.min(...allY);
+  const maxY = Math.max(...allY);
+  
+  chart.y.forEach(line => {
+    const path = createElementNS('path');
+    path.setAttribute('stroke', line.color);
+    path.setAttribute('stroke-width', STROKE_WIDTH);
+    path.setAttribute('stroke-linecap', STROKE_LINECAP);
+    const { data } = line;
+    let d = '';
+    for (let i = 0; i < data.length; i++) {
+      const x = (i / (data.length - 1)) * chartWidth + STROKE_WIDTH;
+      const y = ((maxY - data[i]) / maxY) * chartHeight + STROKE_WIDTH;
+      if (i === 0) {
+        d += `M${x} ${y}`;
+      } else {
+        d += `L${x} ${y}`;
+      }
+    }
+    path.setAttribute('d', d);
+  
+    chartSvg.appendChild(path)
+  });
+  
+  chartWrapperEl.appendChild(chartSvg);
+  app.appendChild(chartWrapperEl);
 }
 
-const sortedData = data.sort((a, b) => a.result - b.result);
+/* UTILS */
+function getColumnByKey(columns, key) {
+  let result;
+  columns.forEach(column => {
+    if (column[0] === key) {
+      result = column.slice(1);
+    }
+  });
+  return result;
+}
 
-const pathData = sortedData.reduce(collectPath, '');
+function createElement(tagName) {
+  return document.createElement(tagName);
+}
 
-const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-path.setAttribute('d', pathData);
-path.setAttribute('stroke', 'red');
-path.setAttribute('stroke-width', 3);
+function createElementNS(tagName) {
+  return document.createElementNS('http://www.w3.org/2000/svg', tagName);
+}
 
-chart.appendChild(path);
+/* TESTING */
+
+function ctt(fn, msg, count = 1) {
+  console.time(msg);
+  for (let c = 0; c < count; c++) {
+    fn();
+  }
+  console.timeEnd(msg);
+}
+
+function pt(fn, msg, count = 1) {
+  const s = performance.now();
+  for (let c = 0; c < count; c++) {
+    fn();
+  }
+  console.log(msg + ':', performance.now() - s);
+}
