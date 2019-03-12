97d8462ca3d612d94906c04c41573f118ee365e6
dubaua <dubaua@gmail.com>
2019-03-12 19:32:40 +0500

    add handles

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const STROKE_WIDTH = 3;
-const STROKE_LINECAP = 'square';
+/* CONSTANTS */
+
+const STROKE_WIDTH = 1.5;
+const STROKE_LINECAP = 'round';
 const STROKE_LINEJOIN = 'round';
 const PATH_ACСURACY = 2; // digits after the decimal point
 const MS_IN_DAY = 1000 * 60 * 60 * 24;
-const app = document.getElementById('chart-app');
+const CHART_MARGIN = 18;
+const CHART_WIDTH = window.innerWidth - CHART_MARGIN * 2;
+const CHART_HEIGHT = 54;
+const ZOOM_CHART_HEIGHT = 100;
 
-const chartWidth = window.innerWidth;
-const chartHeight = 108;
-const zoomChartHeight = 200;
+const app = document.getElementById('chart-app');
 
 createChart(chart_data[0]);
 
@@ -17,6 +20,10 @@ function createChart(chartData) {
   let chart = {
     x: null,
     y: [],
+    range: {
+      start: 0,
+      end: 1,
+    },
   };
 
   for (const key in types) {
@@ -32,24 +39,20 @@ function createChart(chartData) {
             data: getColumnByKey(columns, key),
             color: colors[key],
             name: names[key],
+            active: true,
           });
           break;
       }
     }
   }
 
-  const chartWrapperEl = createElement('section');
-  const chartSvg = createElementNS('svg');
-  chartSvg.setAttribute('width', chartWidth);
-  chartSvg.setAttribute('height', chartHeight);
-  chartSvg.setAttribute('viewBox', [0, 0, chartWidth, chartHeight].join(' '));
-  chartSvg.setAttribute('fill', 'none');
 
-  const zoomChartSvg = createElementNS('svg');
-  zoomChartSvg.setAttribute('width', chartWidth);
-  zoomChartSvg.setAttribute('height', zoomChartHeight);
-  zoomChartSvg.setAttribute('viewBox', [0, 0, chartWidth, chartHeight].join(' '));
-  zoomChartSvg.setAttribute('fill', 'none');
+
+  const chartSvg = createElementNS('svg');
+  chartSvg.setAttribute('width', CHART_WIDTH);
+  chartSvg.setAttribute('height', CHART_HEIGHT);
+  chartSvg.setAttribute('viewBox', [0, 0, CHART_WIDTH, CHART_HEIGHT].join(' '));
+  chartSvg.setAttribute('fill', 'none');
 
   const allY = [].concat(...chart.y.map(line => line.data));
   const maxY = Math.max(...allY);
@@ -63,8 +66,8 @@ function createChart(chartData) {
     const { data } = line;
     let d = '';
     for (let i = 0; i < data.length; i++) {
-      const x = ((i / (data.length - 1)) * (chartWidth - STROKE_WIDTH) + STROKE_WIDTH / 2).toFixed(PATH_ACСURACY);
-      const y = (((maxY - data[i]) / maxY) * (chartHeight - STROKE_WIDTH) + STROKE_WIDTH / 2).toFixed(PATH_ACСURACY);
+      const x = ((i / (data.length - 1)) * (CHART_WIDTH - STROKE_WIDTH) + STROKE_WIDTH / 2).toFixed(PATH_ACСURACY);
+      const y = (((maxY - data[i]) / maxY) * (CHART_HEIGHT - STROKE_WIDTH) + STROKE_WIDTH / 2).toFixed(PATH_ACСURACY);
       if (i === 0) {
         d += `M${x} ${y}`;
       } else {
@@ -75,11 +78,60 @@ function createChart(chartData) {
     chartSvg.appendChild(path);
   });
 
-  chartWrapperEl.appendChild(chartSvg);
+  const chartWrapperEl = createElement('section', 'chart');
+  const chartPreviewEl = createElement('div', 'chart__preview');
+  const chartRangeEl = createElement('div', 'chart__range');
+  const chartHandleEl = createElement('div', 'chart__handle');
+
+
+
+  chartWrapperEl.appendChild(chartPreviewEl);
   app.appendChild(chartWrapperEl);
+
+  const leftLimit = chartPreviewEl.offsetLeft;
+  const chartPreviewWidth = chartPreviewEl.offsetWidth;
+
+  const chartAdjustLeftEl = createElement('div', 'chart__adjust');
+
+  chartAdjustLeftEl.addEventListener('mousedown', function(e) {
+    e.stopPropagation();
+    chartAdjustLeftEl.addEventListener('mousemove', adjustLeft, false);
+  });
+
+  chartAdjustLeftEl.addEventListener('mouseup', function(e) {
+    e.stopPropagation();
+    chartAdjustLeftEl.removeEventListener('mousemove', adjustLeft, false);
+  });
+
+  chartAdjustLeftEl.addEventListener('mouseout', function(e) {
+    e.stopPropagation();
+    chartAdjustLeftEl.removeEventListener('mousemove', adjustLeft, false);
+  });
+
+  chartAdjustLeftEl.addEventListener('mouseleave', function(e) {
+    e.stopPropagation();
+    chartAdjustLeftEl.removeEventListener('mousemove', adjustLeft, false);
+  });
+
+  const adjustLeft = function(e) {
+    const x = getEventX(e);
+    console.log(x);
+    const newPosX = (x - leftLimit) / chartPreviewWidth * 100;
+    // chartRangeEl.style.left = `${newPosX}%`;
+  }
+
+  const chartAdjustRightEl = createElement('div', 'chart__adjust');
+
+  chartPreviewEl.appendChild(chartSvg);
+  chartRangeEl.appendChild(chartAdjustLeftEl);
+  chartRangeEl.appendChild(chartHandleEl);
+  chartRangeEl.appendChild(chartAdjustRightEl);
+  chartPreviewEl.appendChild(chartRangeEl);
+  
 }
 
 /* UTILS */
+
 function getColumnByKey(columns, key) {
   let result;
   columns.forEach(column => {
@@ -90,12 +142,18 @@ function getColumnByKey(columns, key) {
   return result;
 }
 
-function createElement(tagName) {
-  return document.createElement(tagName);
+function createElement(tagName, className) {
+  const el = document.createElement(tagName);
+  el.classList.add(className);
+  return el;
 }
 
 function createElementNS(tagName) {
   return document.createElementNS('http://www.w3.org/2000/svg', tagName);
+}
+
+function getEventX(e) {
+  return e.targetTouches ? e.targetTouches[0].clientX : e.clientX;
 }
 
 /* TESTING */
